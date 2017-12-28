2247b7831381a13eff18bac82bc6a3974b16146b
Subash <subashcbose360@gmail.com>
2017-12-28 14:03:10 +0530

    Adding Error & Chat Page

---
+++ Collaboration_backend/src/main/java/org/com/model/Error.java
@@ -0,0 +1,25 @@
+package org.com.model;
+/*
+ * 	sending error message to the client 
+ */
+public class Error {
+private int code;
+private String message;
+public int getCode() {
+	return code;
+}
+public void setCode(int code) {
+	this.code = code;
+}
+public Error(int code, String message) {
+	super();
+	this.code = code;
+	this.message = message;
+}
+public String getMessage() {
+	return message;
+}
+public void setMessage(String message) {
+	this.message = message;
+}
+}
