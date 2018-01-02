5a757caf7eb5c1330ade9cc961d3957f64091d67
Subash <subashcbose360@gmail.com>
2018-01-02 12:17:02 +0530

    Creating the registrationfrom html format

--- Collaboration_backend/src/main/java/org/com/dao/ProfilePicDAOImpl.java
+++ Collaboration_backend/src/main/java/org/com/dao/ProfilePicDAOImpl.java
@@ -1,5 +1,33 @@
 package org.com.dao;
 
-public class ProfilePicDAOImpl {
+import javax.transaction.Transactional;
+
+import org.com.model.ProfilePicture;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+@Transactional
+public class ProfilePicDAOImpl implements ProfilePictureDAO {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Override
+	public void uploadProfilePic(ProfilePicture profilePicture) {
+		Session session=sessionFactory.getCurrentSession();
+		session.saveOrUpdate(profilePicture); //insert or update
+
+	}
+
+	@Override
+	public ProfilePicture getProfilePic(String username) {
+		Session session=sessionFactory.getCurrentSession();
+		ProfilePicture profilePic=(ProfilePicture)session.get(ProfilePicture.class, username); //select * from profilepic where username=?
+		return profilePic;
+	}
 
 }
